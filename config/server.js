08661d7d7d6d5b113c91470c4f3306d092492352
Jesus Lopez <jesussflr@gmail.com>
2023-09-29 13:51:50 -0400

    server

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env("PUBLIC_URL", "https://crefinex-admin-panel-production.up.railway.app:80"),
+  url: env("PUBLIC_URL", "https://crefinex-admin-panel-production.up.railway.app:1338"),
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
